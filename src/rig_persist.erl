5b494e32b0f9196b92d8bd0e97d1b4ac35389177
Gina Hagg <g.hagg@samsung.com>
2022-02-16 14:59:05 -0800

    add specs

--- src/rig_persist.erl
+++ src/rig_persist.erl
@@ -12,8 +12,8 @@
 -endif.
 
 % API
--export([start_link/0, start_link/1, start_link/2]).
--export([start_link_local/0, start_link_local/1, start_link_local/2, get_country/1, testbool/0]).
+-export([start_link/0]).
+-export([testbool/0]).
 
 % Callbacks
 -export([
@@ -29,39 +29,33 @@
 
 -define(RBDEALS, rb_deals()).
 
-% see: http://erlang.org/doc/man/gen_server.html#start_link-3
-start_link_local() ->
-    start_link_local(#{}).
 
-start_link_local(Args) ->
-    start_link_local(Args, []).
-
-start_link_local(Args, Opts) ->
-    gen_server:start_link({local, ?MODULE}, ?MODULE, Args, Opts).
-
+-spec start_link() ->
+    {ok, pid()}.
 start_link() -> 
    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 rb_deals() ->
     ?GET_ENV(rb_deals, []).
 
-start_link(Args) ->
-    start_link(Args, []).
-
-start_link(Args, Opts) ->
-    gen_server:start_link(?MODULE, Args, Opts).
-
 % Callbacks
-
+-spec init([]) ->
+    {ok, term()}.
 init(_Args) ->
     {ok, #state{}}.
 
+-spec handle_call(term(), pid(),term()) ->
+    {ok, term()}.
 handle_call(_Request, _From, State) ->
     {reply, ignored, State}.
 
+-spec handle_cast(term(), term()) ->
+    {ok, term()}.
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
+-spec handle_info(term(), term()) ->
+    {ok, term()}.
 handle_info({rig_index, update, flights}, State) ->
     {ok,Flights} = rig:all(flights),
     persist_to_cache(Flights),
@@ -70,9 +64,13 @@ handle_info({rig_index, update, flights}, State) ->
 handle_info(_Info, State) ->
     {noreply, State}.
 
+-spec terminate(term(), term()) ->
+    term().
 terminate(_Reason, _State) ->
     ok.
 
+-spec code_change(term(), term(),term()) ->
+    {ok, term()}.
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
@@ -100,11 +98,11 @@ persist_to_cache(Flights) ->
     case RBFlightIds == ExistingRbs of 
         true -> ok;
         _-> 
-        persistent_term:remove({?MODULE, rb_flight_ids}),
+        %persistent_term:remove({?MODULE, rb_flight_ids}),
         persistent_term:put({?MODULE, find_rb_flights}, RBFlightIds),
         RBMap = [#{flight_id => ?LOOKUP(flight_id, Flight, undefined), start_date => ?LOOKUP(startdate, Flight, undefined), 
         deals => ?LOOKUP(deals, Flight, undefined), countries => get_country(Flight)} || Flight <- RBFlights],
-        persistent_term:remove({?MODULE, rb_flights}),
+        %persistent_term:remove({?MODULE, rb_flights}),
         persistent_term:put({?MODULE, rb_flights}, RBMap),
         error_logger:info_msg("rb_flights: ~p~n",[persistent_term:get({?MODULE, rb_flights},[])])
     end.
@@ -130,7 +128,7 @@ andsplits([BfreAnd, _], _) ->
 extract_country(Tokens) ->
     [C || {_, _, C} = Token <- Tokens, element(1,Token) == string].
  
-
+-spec testbool() -> tuple().
 testbool() -> 
     B1 = <<"((tvidlist_ids all of (10952,12895,23299)) and (matched_tv) and ((impression_type <> 'app' or applist_ids none of (573,828,2435))) and (((not private) or (exchange = 6 and (\"ADG10365AEN\" in deal_ids or \"ADG10336FUN\" in deal_ids or \"ADG10322FUS\" in deal_ids or \"ADG10321JUK\" in deal_ids or \"ADG10313DIG\" in deal_ids or \"ADG10312DIG\" in deal_ids or \"ADG10314TAS\" in deal_ids or \"ADG10301OUT\" in deal_ids or \"ADG10309JUK\" in deal_ids or \"ADG10310THI\" in deal_ids or \"ADG10311DAN\" in deal_ids or \"ADG10305XUM\" in deal_ids or \"ADG10318HAY\" in deal_ids or \"ADG10319HAY\" in deal_ids or \"ADG10335FUN\" in deal_ids or \"ADG10368HAY\" in deal_ids)) or (exchange = 15 and (\"181e7.5cf31.91fb\" in deal_ids or \"3de16.2c733.3d32\" in deal_ids or \"48f74.51e52.8141\" in deal_ids)))) and ((dma <> 623 and dma <> 770 and dma <> 539 and dma <> 560 and dma <> 616 and dma <> 510)) and (country = \"US\") and ((device_type_id = 4 or device_type_id = 5))) and ((within_frequency_cap(\"campaign:ip\", \"3561301\", 3, 86400) and within_frequency_cap(\"campaign:ip\", \"3561302\", 2, 3600)))">>,
     B2 = <<"(((impression_type <> 'app' or applist_ids one of (1387))) and (((not private) or (exchange = 15 and (\"39e5b.905c9.e784\" in deal_ids)))) and (country in (\"CA\",\"US\")))">>,
